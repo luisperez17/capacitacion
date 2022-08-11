2a84ec138cbbe15e484af5639a7c14c7d70dfbc6
Juan Carlos Osorio <juancaom12@gmaol.com>
2022-08-10 19:13:26 -0500

    carruseles listos

--- web/modules/custom/lib_components_v3/lib_components_v3/js/index.js
+++ web/modules/custom/lib_components_v3/lib_components_v3/js/index.js
@@ -1,22 +1,27 @@
-/* eslint-disable */
-Drupal.behaviors.libCarrousel = {
-    attach(context) {
-  
-      var carrouselitem = document.querySelectorAll('.lib-comp-carrousel__item');
-      var carrouselnav = document.querySelectorAll('.lib-comp-carrousel__circle');
+const parentCarousel = document.getElementsByClassName('lib-comp-carrousel')
 
-      for (let i = 0; i < carrouselnav.length; i++) {
-        carrouselnav[i].addEventListener("click", function(event) {
-          event.preventDefault();
-          for (let q = 0; q < carrouselitem.length; q++) {
-            const element = carrouselitem[q];
-            if ( element.classList.contains('is-active')) {
-              element.classList.remove('is-active');
-            }
+
+for(let i=0; i<parentCarousel.length; i++){
+  const elements = parentCarousel[i];
+  let circleAction = elements.querySelectorAll('.lib-comp-carrousel__circle')  
+  for(let j=0; j<circleAction.length; j++){
+    let circleActionSingle = circleAction[j];
+    let number = j+1;
+    circleActionSingle.addEventListener('click', function(e){
+      let clickAction= circleActionSingle.closest('.lib-comp-carrousel').querySelectorAll('.lib-comp-carrousel__items');
+      for(let t=0; t<clickAction.length; t++){
+        let actionSingle = clickAction[t];
+        let item = actionSingle.querySelectorAll('.lib-comp-carrousel__item');
+        for(let y=0; y<item.length; y++){
+          item[y].classList.remove('is-active');
+          let classEdit = `lib-comp-carrousel__item__${number}`;
+          if(item[y].classList.contains(classEdit)){
+            item[y].classList.add('is-active');
           }
-          carrouselitem[i].classList.add('is-active');
-        });
-      };
-    },
-  };
+          
+        }
+      }
+    })
+  }
+}
   
