cf7793cc21c6e52b8780ddbf3e965c25814bb155
Julian Casas <julian.casas@esinergia.co>
2023-11-29 17:44:25 -0500

    Add custom template to 'ewn_theme_julian' to admin display of news julian contentype generated.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -778,3 +778,9 @@ if (file_exists(__DIR__ . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
+/**
+ * Development mode settings.
+ */
+if (file_exists($app_root . '/modules/contrib/dev_mode/settings.dev_mode.php')) {
+  include $app_root . '/modules/contrib/dev_mode/settings.dev_mode.php';
+}
---
+++ web/themes/new_theme_julian/js/new-theme-julian.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * New theme Julian behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.newThemeJulian = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
